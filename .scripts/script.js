10f9ecf441901c488803234884a30192dd6a648c
Murray Jack <mjack@starrez.com>
2022-11-07 17:18:26 +1100

    fix: ci changes

--- .scripts/script.js
+++ .scripts/script.js
@@ -3,55 +3,65 @@ const fs = require("fs")
 console.log("Manifest script running");
 
 const package = require("../package.json");
-const minVersion = package["min-srw-version"]
+const minVersion = package["min-srw-version"];
 
-// load the original one
-let data = {
-    date: (new Date()).toISOString(),
-    apps: [],
-    versions: [],
-}
-
-if (!fs.existsSync("./dist", {folders: true})) {
-    console.log("nothing todo")
-    return;
-}
-
-if (fs.existsSync("./dist/manifest.json")) {
-    const json = fs.readFileSync("./dist/manifest.json", 'utf8')
-    data = JSON.parse(json)
-}
-
-// loop thru the dis/apps folder to get
-const folders = fs.readdirSync("./dist/apps", {folders: true})
-
-folders.forEach(app => {
-    console.log(app)
-    
-    const main = fs.readdirSync(`./dist/apps/${app}`).filter(e => e.startsWith("main.") && e.endsWith(".js"))[0]
-    const polyfill = fs.readdirSync(`./dist/apps/${app}`).filter(e => e.startsWith("polyfills.") && e.endsWith(".js"))[0]
-
-    // add the app, if we don't already have one
-    if (!data.apps.some(e => e === app)) {
-        data.apps.push(app)
-    }
-    
-    if (!data.versions.some(e => e === minVersion)) {
-        data.versions.push(minVersion)
-    }
-    
-    // add the main and the polly fill to this version number
-    data[minVersion] = {
-       [app]: {
-            main,
-            polyfill
-       }
+const getPreviousData = () => {
+    let data = {
+        date: (new Date()).toISOString(),
+        apps: [],
+        versions: [],
     }
 
-});
+    if (!fs.existsSync("./dist", {folders: true})) {
+        console.log("nothing todo")
+        return;
+    }
+    
+    if (fs.existsSync("./dist/manifest.json")) {
+        const json = fs.readFileSync("./dist/manifest.json", 'utf8')
+        data = JSON.parse(json)
+    }    
+}
 
-fs.writeFileSync("./dist/manifest.json", JSON.stringify(data, undefined, 2))
+const saveVersionJSON = (data) => {
+    fs.writeFileSync("./dist/manifest.json", JSON.stringify(data, undefined, 2))
+}
+
+(() => {
+    // load the original one
+    const data = getPreviousData();
+
+    // loop thru the dis/apps folder to get
+    const folders = fs.readdirSync("./dist/apps", {folders: true})
+
+    folders.forEach(app => {
+        console.log(app)
+        
+        const main = fs.readdirSync(`./dist/apps/${app}`).filter(e => e.startsWith("main.") && e.endsWith(".js"))[0]
+        const polyfill = fs.readdirSync(`./dist/apps/${app}`).filter(e => e.startsWith("polyfills.") && e.endsWith(".js"))[0]
+
+        // add the app, if we don't already have one
+        if (!data.apps.some(e => e === app)) {
+            data.apps.push(app)
+        }
+        
+        // add the version
+        if (!data.versions.some(e => e === minVersion)) {
+            data.versions.push(minVersion)
+        }
+        
+        // add the main and the polly fill to this version number
+        data[minVersion] = {
+            [app]: {
+                    main,
+                    polyfill
+            }
+        }
+
+    });
+
+    saveVersionJSON(data);
+})()
 
 console.log(JSON.stringify(data, undefined, 2))
-
 console.log("Manifest script: Complete");
